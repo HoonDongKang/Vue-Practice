0719d5e801f6349f1a9fcb0f220acaca44a70c85
HoonDongKang <d159123@naver.com>
2023-10-13 23:17:27 +0900

    vue cli form

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <form @submit="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input type="text" id="username" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input type="password" id="password" v-model="password">
+    </div>password
+    <button type="submit">Login</button>
+
+  </form>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      username:'',
+      password:'',
+
+    }
+  },
+  methods:{
+    submitForm(event){
+      event.preventDefault()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli/src/AppTwo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!-- <app-header v-bind:child comp prop name="parent component prop"/> -->
+    <app-header v-bind:propsdata="str" @renew='renewStr'/>
+    {{str}}
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  components:{
+    'app-header': AppHeader
+  },
+  data(){
+    return{
+      str:"Header"
+    }
+  },
+  methods:{
+    renewStr(){
+      this.str = 'hi'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
