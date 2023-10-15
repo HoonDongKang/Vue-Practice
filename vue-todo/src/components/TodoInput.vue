c4f74bf1cce0dda2ab3d9277465e79c864e6cfeb
HoonDongKang <d159123@naver.com>
2023-10-16 00:01:03 +0900

    todo vue cli

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem">
+    <button @click="addTodo">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      newTodoItem:""
+    }
+  },
+  methods:{
+    addTodo(){
+      localStorage.setItem(this.newTodoItem, this.newTodoItem)
+      this.clearInput()
+    },
+    clearInput(){
+      this.newTodoItem=''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(todoItem, index) in todoItems" :key="index">{{todoItem}}
+        <button @click="removeTodo(todoItem, index)">remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      todoItems:[]
+    }
+  },
+  created(){
+    if(localStorage.length>0){
+      for(var i= 0; i<localStorage.length; i++){
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+          this.todoItems.push(localStorage.key(i))
+        }
+      }
+    }
+  },
+  methods:{
+    removeTodo(todoItem, index){
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index,1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
