b46e2801f4b6ee401f7161e2f8bf6d89c0e84ff6
HoonDongKang <d159123@naver.com>
2023-11-11 23:23:55 +0900

    .

--- vue-news/src/store/actions.js
+++ vue-news/src/store/actions.js
@@ -1,21 +1,21 @@
 import { fetchUserInfo, fetchItemInfo, fetchList } from '../api'
 export default{
   FETCH_USER({ commit }, name){
-    fetchUserInfo(name)
+    return fetchUserInfo(name)
       .then(res => {
         commit('SET_USER', res.data);
       })
       .catch(err => console.error(err))
   },
   FETCH_ITEM({ commit }, id){
-    fetchItemInfo(id)
+    return fetchItemInfo(id)
       .then(res => {
         commit('SET_ITEM', res.data);
       })
       .catch(err => console.error(err));
   },
   FETCH_LIST({ commit }, pageName){
-    fetchList(pageName)
+    return fetchList(pageName)
       .then(res => {
         commit('SET_LIST', res.data);
       })
